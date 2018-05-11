957f3c74dd43f8de6298f924491a5802b913f3a7
PC-27\user <ptanh2212@gmail.com>
2018-05-11 11:39:08 +0700

    Commit 11/5/2018

---
+++ Assets/Scripts/Utilities/ObjectFactory.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//ATTACH TO ObjectFactory gameObject
+public class ObjectFactory: MonoBehaviour {
+
+	public static ObjectFactory instance;
+
+	void Awake()
+	{
+		instance = this;
+	}
+
+	void Start () {
+	}
+
+	public enum PrefabType
+	{
+		None,
+		Enemy,
+		AudioSource,
+		Explosion1,
+		Explosion2,
+		Explosion3,
+		Explosion4,
+		ScoreAddText
+	}
+
+	public Dictionary<PrefabType,string> PrefabPaths = new Dictionary<PrefabType, string> {
+		
+		{ PrefabType.None, "" },
+		{ PrefabType.Enemy, "Prefabs/Enemy" },
+		{ PrefabType.AudioSource, "Prefabs/AudioSource" },
+		{ PrefabType.Explosion1, "Prefabs/Explosion1" },
+		{ PrefabType.Explosion2, "Prefabs/Explosion2" },
+		{ PrefabType.Explosion3, "Prefabs/Explosion3" },
+		{ PrefabType.Explosion4, "Prefabs/Explosion4" },
+		{ PrefabType.ScoreAddText, "Prefabs/ScoreAddText" },
+	};
+
+	public GameObject MakeObject(PrefabType type)
+	{
+		string path;
+		if (PrefabPaths.TryGetValue (type, out path)) {
+			return (Instantiate (Resources.Load (path, typeof(GameObject))) as GameObject);
+		}
+		print ("NULL");
+		return null;
+	}
+
+}
---
+++ Assets/Scripts/Utilities/ObjectPool.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//ATTACH TO ObjectPool gameObject
+
+public class ObjectPool : MonoBehaviour {
+
+//	public static ObjectPool instance;
+//
+//	GenericObject<Enemy> enemy;
+//
+//	GenericObject<Explosion> explosion1;
+//	GenericObject<Explosion> explosion2;
+//	GenericObject<Explosion> explosion3;
+//	GenericObject<Explosion> explosion4;
+//	GenericObject<AddScoreText> addScoreText;
+//
+//	void Awake()
+//	{
+//		instance = this;
+//	}
+//
+//	// Use this for initialization
+//	void Start () {
+//		enemy = new GenericObject<Enemy>(ObjectFactory.PrefabType.Enemy,20);
+//		explosion1 = new GenericObject<Explosion>(ObjectFactory.PrefabType.Explosion1,3);
+//		explosion4= new GenericObject<Explosion>(ObjectFactory.PrefabType.Explosion4,3);
+//		addScoreText= new GenericObject<AddScoreText>(ObjectFactory.PrefabType.ScoreAddText,1);
+//	}
+//		
+//
+//	public Enemy GetEnemy()
+//	{
+//		return enemy.GetObj ();
+//	}
+//		
+//	public Explosion GetExplosion1()
+//	{
+//		return explosion1.GetObj ();
+//	}
+//
+//	public Explosion GetExplosion2()
+//	{
+//		return explosion2.GetObj ();
+//	}
+//
+//	public Explosion GetExplosion3()
+//	{
+//		return explosion3.GetObj ();
+//	}
+//
+//	public Explosion GetExplosion4()
+//	{
+//		return explosion4.GetObj ();
+//	}
+//
+//	public AddScoreText GetAddScoreText()
+//	{
+//		return addScoreText.GetObj ();
+//	}
+}
