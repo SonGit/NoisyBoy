fbd43fb17de25f0bf6f7843dfc39619d98b26a2e
PC-27\user <ptanh2212@gmail.com>
2018-05-16 14:55:00 +0700

    16/5/2018

---
+++ Assets/Scripts/Doorway.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doorway : Door {
+
+	public Transform DoorStartPoint;
+	public Transform DoorEndPoint;
+	public bool left;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		base.Update ();
+
+	}
+
+	public override void Open ()
+	{
+		if (left) {
+			targetRot = Quaternion.Euler (0, -90, 0);
+		} else {
+			targetRot = Quaternion.Euler (0, 90, 0);
+		}
+	}
+
+	public override void Close ()
+	{
+		targetRot = Quaternion.Euler(0, 0, 0);
+	}
+}
